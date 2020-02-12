1d06eb2dc154783f61f95dbbc00110c062a5f053
Katie Belanger <belangerkat@gmail.com>
2020-02-12 02:48:21 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,7 +18,7 @@ def third_challenge
         #your key/value pairs here
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         #your key/value pairs here
@@ -40,5 +40,5 @@ def third_challenge
    }
   }
 
-  
+
 end
